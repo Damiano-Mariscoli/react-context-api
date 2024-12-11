2f037cc20f5475e0bc97ee07c80d72c54087c85e
Damiano-Mariscoli <damiano.mariscoli2@gmail.com>
2024-12-11 17:39:31 +0100

    ADDED: funzione my counter di prova

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,30 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./HomePage.jsx";
 import { Content } from "./Content.jsx";
 import { ChiSiamo } from "./ChiSiamo.jsx";
-
+import CountContext from "../context/PostContext.jsx";
+import { useContext } from "react";
 import DefaultLayout from "./DefaultLayout.jsx";
 import SinglePosts from "./components/SinglePost.jsx";
 
 function App() {
+  function MyCounter() {
+    const { count } = useContext(CountContext);
+    return <div>{count}</div>;
+  }
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<DefaultLayout />}>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/chi-siamo" element={<ChiSiamo />} />
-          <Route path="/posts" element={<Content />} />
-          <Route path="/posts/:id" element={<SinglePosts />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <CountContext.Provider value={{ count: 1 }}>
+      <MyCounter />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<DefaultLayout />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/chi-siamo" element={<ChiSiamo />} />
+            <Route path="/posts" element={<Content />} />
+            <Route path="/posts/:id" element={<SinglePosts />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </CountContext.Provider>
   );
 }
 
